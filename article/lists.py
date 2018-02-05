ab0573db1cf5f011273035b434e7452f1d36eb06
hyeonik7 <hyeonik7@gmail.com>
2018-02-05 20:05:16 +0900

    prpress_list make as set

--- article/lists.py
+++ article/lists.py
@@ -8,7 +8,7 @@ year_list = ['2015','2016','2017','2018']
 month_list = ['1','2','3','4','5','6','7','8','9','10','11','12']
 day_list = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
 press_list = []
-category_list = ['정치', '경제', '사회', '생활/문화', '세계', 'IT/과학', '오피니언']
+category_list = ['정치', '경제', '사회', '생활/문화', '세계', 'IT/과학']
 
 try:
     press_list.extend(list(PoliticsDocument.objects.values_list("press", flat=True).distinct()))
@@ -17,6 +17,9 @@ try:
     press_list.extend(list(CultureLivingDocument.objects.values_list("press", flat=True).distinct()))
     press_list.extend(list(WorldDocument.objects.values_list("press", flat=True).distinct()))
     press_list.extend(list(ITScienceDocument.objects.values_list("press", flat=True).distinct()))
+    press_set = set(press_list)
+    press_list = list(press_set)
+    press_list.sort()
 
 except Exception as e:
     press_list = ['조선일보', '중앙일보', '동아일보', '경향신문', '한겨레', '한국경제', '매일경제']
@@ -28,3 +31,6 @@ else:
     press_list.extend(list(CultureLivingDocument.objects.values_list("press", flat=True).distinct()))
     press_list.extend(list(WorldDocument.objects.values_list("press", flat=True).distinct()))
     press_list.extend(list(ITScienceDocument.objects.values_list("press", flat=True).distinct()))
+    press_set = set(press_list)
+    press_list = list(press_set)
+    press_list.sort()
